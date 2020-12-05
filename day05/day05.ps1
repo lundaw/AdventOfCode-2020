61e997ee7b4e5096463f8e70b227c1be35cd57c0
Kovács Tamás <kt@famulus.hu>
2020-12-05 21:25:23 +0100

    Day 5 PowerShell solution

---
+++ day05/day05.ps1
@@ -0,0 +1,14 @@
+﻿$seatsData = Get-Content "seats.txt" | ForEach-Object {
+    # $_ is the pipeline element
+    # 2 is the fromBase argument
+    [convert]::ToInt32(($_ -replace "B|R",1 -replace "F|L",0), 2)
+} | Sort-Object # and also sort in ascending order
+
+# Since the values are in ascending order, we can index the last element now to get the
+# result for the largest seat ID.
+Write-Host ("Largest seat ID: " + $seatsData[-1])
+
+# To get the missing seat ID, generate a range from the smallest to the largest and
+# get the differnce of the two.
+$missing = $seatsData[0]..$seatsData[-1] | where {$_ -notin $seatsData}
+Write-Host ("Missing seat ID: " + $missing)
