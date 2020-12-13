6e28ec17949460a07f6b5c3e0c153928fcaf0c3d
Kovács Tamás <kt@famulus.hu>
2020-12-13 16:39:50 +0100

    Simplify Day 1 python

--- day01/day01.py
+++ day01/day01.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
+from math import prod
+from itertools import combinations
 
 def readInput(filename):
     file = open(filename, "r")
-    data = file.readlines()
-    data = [int(value) for value in data]
+    data = [int(value) for value in file.readlines()]
 
     return data
 
 
-def partOne(data):
-    for i in data:
-        for j in data:
-            if (i+j) == 2020:
-                print("[Part One]: {:d}".format(i*j))
-                return
-
-
-def partTwo(data):
-    for i in data:
-        for j in data:
-            for k in data:
-                if i+j+k == 2020:
-                    print("[Part Two]: {:d}".format(i*j*k))
-                    return
+def solve(elements):
+    for combination in combinations(data, elements):
+        if sum(combination) == 2020:
+            return prod(combination)
 
 
 data = readInput("input.txt")
-partOne(data)
-partTwo(data)
+print("[Part 1]: {:d}".format(solve(2)))
+print("[Part 2]: {:d}".format(solve(3)))
